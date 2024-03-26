66c36dd6d81903b2d5b3add81ea821df176042b4
aschro13 <158303408+aschro13@users.noreply.github.com>
2024-03-26 15:12:49 -0400

    added JUnit test stubs for testTeam

---
+++ Team.java
@@ -0,0 +1,51 @@
+/*
+ * Programmer: Adam Schroeder
+ * Project:    Final Project - Fantasy Baseball
+ * Course:     COSC 381, Sp '24
+ *  
+ */
+
+package main;
+
+import java.util.*;
+import java.io.*;
+
+public class Team {
+	//included data members
+	private ArrayList<Player> roster;
+	//private ArrayList<Player> freeAgents;  //These will be removed and added to the Driver class as new Team objects
+	//private ArrayList<Player> allPlayers;  // ^               ^                         ^                          ^
+	private int teamOwner;
+	private String teamName;
+	
+
+//***********************************METHODS********************************************
+	//method to list the entire roster of the current team
+	public void listRoster() {
+	
+		System.out.println("It's a-me! Roster List!");
+		//for (int i = 0; i < roster.size(); i++) {
+			//System.out.println(i + ": " + roster[i].name);      //check Player class for either data member or toString to pull the name
+		//} //end for loop
+}//end listRoster
+	
+	//method to identify a player as being drafted by another team
+	public void oDraft() {
+		System.out.println("oDraft completed");
+	}//end oDraft
+	
+	//method to draft a player onto current team
+	public void iDraft() {
+		System.out.println("iDraft completed");
+	}
+	
+	//method to release a player as a Free Agent
+	public void release() {
+		System.out.println("Player released");
+	}
+	
+	
+	
+	
+	
+}
--- testTeam.java
+++ testTeam.java
@@ -1,3 +1,25 @@
+import static orc.junit.Assert.*;
+
 public class testTeam {
-    
+ 
+	@Test
+	public void testListRoster() {
+		assertEquals(true);
+	}
+	
+	@Test
+	public void testIDraft() {
+		assertEquals(true);
+	}
+	
+	@Test
+	public void testODraft() {
+		assertEquals(true);
+	}
+	
+	@Test
+	public void testRelease() {
+		assertEquals(true);
+	}
+	
 }
